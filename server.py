d2885dc2f34555bae6da408fca6fb2cd6001aeeb
HRY <toto850105@gmail.com>
2018-01-15 23:47:53 +0800

    change web server to python flask

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import render_template
+import os
+
+cwd = os.getcwd()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/buy")
+def buy():
+    return render_template("buy.html")
+
+@app.route("/need")
+def need():
+    return render_template("need.html")
+
+@app.route("/menuall")
+def menuall():
+    return render_template("menu-all.html")
+
+@app.route("/menu1")
+def menu1():
+    return render_template("menu1.html")
+
+@app.route("/menu2")
+def menu2():
+    return render_template("menu2.html")
+
+@app.route("/menu3")
+def menu3():
+    return render_template("menu3.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
