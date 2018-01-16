cf8527dd95c7f8263926c7e3c569d0dce14b62df
HRY <toto850105@gmail.com>
2018-01-16 09:28:12 +0800

    remove somoe server route

--- server.py
+++ server.py
@@ -23,22 +23,6 @@ def buy():
 def need():
     return render_template("need.html")
 
-@app.route("/menuall")
-def menuall():
-    return render_template("menu-all.html")
-
-@app.route("/menu1")
-def menu1():
-    return render_template("menu1.html")
-
-@app.route("/menu2")
-def menu2():
-    return render_template("menu2.html")
-
-@app.route("/menu3")
-def menu3():
-    return render_template("menu3.html")
-
 @app.route("/post", methods=["POST"])
 def post():
     dd = request.form['data']
